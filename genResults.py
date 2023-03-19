c13426966fa2806cb60352ec2e8be604b015242a
Hooman Sherkat <3044447+hsherkat@users.noreply.github.com>
2023-03-19 14:38:03 -0700

    R - move scores initialization

--- genResults.py
+++ genResults.py
@@ -19,8 +19,7 @@ def main():
     correct_picks, points_available = generate_picks_and_points(game_results)
 
     user_names, user_picks = read_user_brackets()
-    scores = {}
-    determine_results(correct_picks, points_available, scores, user_names, user_picks)
+    scores = determine_results(correct_picks, points_available, user_names, user_picks)
     write_results(scores)
 
 
@@ -42,32 +41,42 @@ def write_results(scores):
             )
 
 
-def determine_results(correct_picks, points_available, scores, user_names, user_picks):
+def determine_results(correct_picks, points_available, user_names, user_picks):
+    scores = {}
+
     with open("userResultsPy.dat", "w") as fh:
         for user, picks in zip(user_names, user_picks):
-            score = 0
             # 0 = no result, 1 = correct, 2 = incorrect
-            returnS = [0] * 63
-            for game_idx in range(63, 0, -1):
-                if correct_picks[game_idx - 1] > 0:
-                    # Wrong pick
-                    if correct_picks[game_idx - 1] != float(picks[game_idx - 1]):
-                        returnS[game_idx - 1] = 2
-                    # Right pick in the first round
-                    elif game_idx > 31:
-                        returnS[game_idx - 1] = 1
-                    # Second round or later
-                    else:
-                        # Only correct if the user also match in the earlier round
-                        if returnS[2 * game_idx + correct_picks[game_idx - 1] - 2] == 1:
-                            returnS[game_idx - 1] = 1
-                        else:
-                            returnS[game_idx - 1] = 2
-            for ll in range(len(returnS)):
-                score = score + (returnS[ll] % 2) * points_available[ll]
-                returnS[ll] = str(returnS[ll])
-            scores[user] = score
-            fh.write("".join(returnS) + "\n")
+            returnS = process_picks(correct_picks, picks)
+            scores[user] = calculate_score(points_available, returnS)
+
+            fh.write("".join(map(str, returnS)) + "\n")
+
+    return scores
+
+
+def process_picks(correct_picks, picks):
+    returnS = [0] * 63
+    for game_idx in range(63, 0, -1):
+        if correct_picks[game_idx - 1] > 0:
+            # Wrong pick
+            if correct_picks[game_idx - 1] != float(picks[game_idx - 1]):
+                returnS[game_idx - 1] = 2
+            # Right pick in the first round
+            elif game_idx > 31:
+                returnS[game_idx - 1] = 1
+            # Second round or later
+            else:
+                # Only correct if the user also match in the earlier round
+                if returnS[2 * game_idx + correct_picks[game_idx - 1] - 2] == 1:
+                    returnS[game_idx - 1] = 1
+                else:
+                    returnS[game_idx - 1] = 2
+    return returnS
+
+
+def calculate_score(points_available, returnS):
+    return sum((retS % 2) * points for retS, points in zip(returnS, points_available))
 
 
 def read_user_brackets():
