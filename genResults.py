bc51e0b833dceb9411724e4586911d714e1b15de
Hooman Sherkat <3044447+hsherkat@users.noreply.github.com>
2023-03-19 13:48:21 -0700

    r - extract method

--- genResults.py
+++ genResults.py
@@ -30,33 +30,51 @@ def main():
     scores, userNames, userPicks = read_user_brackets()
 
     # Determine results
-    fh = open("userResultsPy.dat", "w")
-    scoreFile = open("userScores.dat", "w")
-    for ii in range(len(userPicks)):
-        score = 0
-        # 0 = no result, 1 = correct, 2 = incorrect
-        returnS = [0] * 63
-        myP = userPicks[ii]
-        for iGame in range(63, 0, -1):
-            if CP[iGame - 1] > 0:
-                # Wrong pick
-                if CP[iGame - 1] != float(myP[iGame - 1]):
-                    returnS[iGame - 1] = 2
-                # Right pick in the first round
-                elif iGame > 31:
-                    returnS[iGame - 1] = 1
-                # Second round or later
-                else:
-                    # Only correct if the user also match in the earlier round
-                    if returnS[2 * iGame + CP[iGame - 1] - 2] == 1:
-                        returnS[iGame - 1] = 1
-                    else:
+    determine_results(CP, PA, scores, userNames, userPicks)
+
+    write_results(scores)
+
+
+def write_results(scores):
+    with open("userScores.dat", "w") as scoreFile:
+        for teamValPair in sorted(scores.items(), key=operator.itemgetter(1), reverse=True):
+            scoreFile.write(
+                teamValPair[0] + " with " + str(teamValPair[1]) + " points" + "\n"
+            )
+
+
+def determine_results(CP, PA, scores, userNames, userPicks):
+    with open("userResultsPy.dat", "w") as fh:
+        for ii in range(len(userPicks)):
+            score = 0
+            # 0 = no result, 1 = correct, 2 = incorrect
+            returnS = [0] * 63
+            myP = userPicks[ii]
+            for iGame in range(63, 0, -1):
+                if CP[iGame - 1] > 0:
+                    # Wrong pick
+                    if CP[iGame - 1] != float(myP[iGame - 1]):
                         returnS[iGame - 1] = 2
-        for ll in range(len(returnS)):
-            score = score + (returnS[ll] % 2) * PA[ll]
-            returnS[ll] = str(returnS[ll])
-        scores[userNames[ii]] = score
-        fh.write("".join(returnS) + "\n")
+                    # Right pick in the first round
+                    elif iGame > 31:
+                        returnS[iGame - 1] = 1
+                    # Second round or later
+                    else:
+                        # Only correct if the user also match in the earlier round
+                        if returnS[2 * iGame + CP[iGame - 1] - 2] == 1:
+                            returnS[iGame - 1] = 1
+                        else:
+                            returnS[iGame - 1] = 2
+            for ll in range(len(returnS)):
+                score = score + (returnS[ll] % 2) * PA[ll]
+                returnS[ll] = str(returnS[ll])
+            scores[userNames[ii]] = score
+            fh.write("".join(returnS) + "\n")
+
+
+def read_user_brackets():
+    fh = open("userbrackets.dat", "r")
+    game_results = fh.read().splitlines()
     fh.close()
     for teamValPair in sorted(scores.items(), key=operator.itemgetter(1), reverse=True):
         scoreFile.write(
